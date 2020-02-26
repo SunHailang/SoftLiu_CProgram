27d96a42207322a4d71b1f3b6cf0d05bd13f3ccf
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-02-26 19:54:54 +0800

    update

--- EmptyMethod/Src/SortUtils.cpp
+++ EmptyMethod/Src/SortUtils.cpp
@@ -1,15 +1,26 @@
-﻿#include "../stdafx.h"
+﻿#include <stdio.h>
 
-#pragma region 冒泡排序
-void bubble_sort(int arr[], int len) {
+#pragma region 冒泡排序 默认升序
+void bubble_sort(int arr[], int len, bool sortMode = true)
+{
 	int i, j, temp;
 	for (i = 0; i < len - 1; i++)
+	{
 		for (j = 0; j < len - 1 - i; j++)
-			if (arr[j] > arr[j + 1]) {
+		{
+			bool mode = arr[j] > arr[j + 1];
+			if (!sortMode)
+			{
+				mode = arr[j] < arr[j + 1];
+			}
+			if (mode)
+			{
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 			}
+		}
+	}
 }
 #pragma endregion
 
@@ -29,8 +40,12 @@ void selection_sort(int arr[], int len)
 	{
 		int min = i;
 		for (j = i + 1; j < len; j++)     //走访未排序的元素
+		{
 			if (arr[j] < arr[min])    //找到目前最小值
+			{
 				min = j;    //记录最小值
+			}
+		}
 		swap(&arr[min], &arr[i]);
 	}
 }
@@ -40,10 +55,13 @@ void selection_sort(int arr[], int len)
 void insertion_sort(int arr[], int len)
 {
 	int i, j, temp;
-	for (i = 1; i < len; i++) {
+	for (i = 1; i < len; i++)
+	{
 		temp = arr[i];
 		for (j = i; j > 0 && arr[j - 1] > temp; j--)
+		{
 			arr[j] = arr[j - 1];
+		}
 		arr[j] = temp;
 	}
 }
@@ -56,10 +74,13 @@ void shell_sort(int arr[], int len)
 	int temp;
 	for (gap = len >> 1; gap > 0; gap = gap >> 1)
 	{
-		for (i = gap; i < len; i++) {
+		for (i = gap; i < len; i++)
+		{
 			temp = arr[i];
 			for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+			{
 				arr[j + gap] = arr[j];
+			}
 			arr[j + gap] = temp;
 		}
 	}
